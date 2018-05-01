e7a6525fe09e6f9eb4f63940551db01bdc8866ed
Welinton Ribeiro Junior <juniortrimundial@hotmail.com>
2018-05-01 01:17:24 -0300

    v1.2

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ app.get('/', (req, res) => {
   res.send('get 1 \n');
 });
 
-const routes = require('/routes/api');
-app.use('/api', routes);
+//const routes = require('/routes/api');
+app.get('/api', (req, res) =>{
+	res.send('get 2');
+});
+
+app.post('/api/post', (req, res) =>{
+	res.send('POST 1');
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
